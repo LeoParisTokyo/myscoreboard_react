5687d5b6579a9c2e241c7bd068185a815e6ebac2
Leonard Mouillet <leonard_mouillet@hotmail.com>
2018-02-08 18:11:27 +0100

    added state management for the Counter component

--- app.jsx
+++ app.jsx
@@ -31,15 +31,33 @@ Header.propTypes = {
 
 var Counter = React.createClass({
   propTypes: {
-    score: React.PropTypes.number.isRequired,
+    initialScore: React.PropTypes.number.isRequired,
+  },
+
+  getInitialState: function(){
+    return {
+      score: this.props.initialScore,
+    };
+  },
+
+  incrementScore: function (){
+    this.setState({
+      score: (this.state.score + 1),
+    });
+  },
+
+  decrementScore: function (){
+    this.setState({
+      score: (this.state.score - 1),
+    });
   },
 
   render: function() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {this.props.score} </div>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <div className="counter-score"> {this.state.score} </div>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
@@ -54,7 +72,7 @@ function Player(props){
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter initialScore={props.score}/>
       </div>
     </div>
   );
